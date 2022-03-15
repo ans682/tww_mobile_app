846f923b8cb8ee773fdd24a74fa07ec2bfdc64a1
Alem <ans682@nyu.edu>
2022-03-15 14:01:59 +0600

    finished UI of login page

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,13 +1,15 @@
-// import 'dart:html';
+//import 'dart:html';
 
 import 'package:flutter/material.dart';
+import 'package:tww_mobile_app_v1/screens/home_screen.dart';
+import 'package:tww_mobile_app_v1/screens/registration_screen.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
 
-  @override 
+  @override
   _LoginScreenState createState() => _LoginScreenState();
-} 
+}
 
 class _LoginScreenState extends State<LoginScreen> {
   // form key to validate user email
@@ -17,56 +19,46 @@ class _LoginScreenState extends State<LoginScreen> {
   final TextEditingController emailController = new TextEditingController();
   final TextEditingController passwordController = new TextEditingController();
 
-
-  @override 
+  @override
   Widget build(BuildContext context) {
     // email field
     final emailField = TextFormField(
-      autofocus: false,
-      controller: emailController,
-      keyboardType: TextInputType.emailAddress,
-      // validator: () {},
-      onSaved: (value)
-      {
-        emailController.text = value!;
-      },
-      textInputAction: TextInputAction.next,
-      decoration: InputDecoration(
-        prefixIcon: Icon(Icons.mail),
-        contentPadding: EdgeInsets.fromLTRB(20,15,20,15),
-        hintText: "Email",
-        border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(10),
+        autofocus: false,
+        controller: emailController,
+        keyboardType: TextInputType.emailAddress,
+        // validator: () {},
+        onSaved: (value) {
+          emailController.text = value!;
+        },
+        textInputAction: TextInputAction.next,
+        decoration: InputDecoration(
+          prefixIcon: Icon(Icons.mail),
+          contentPadding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+          hintText: "Email",
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
           ),
-
-      )
-    );
+        ));
 
     // password field
     final passwordField = TextFormField(
-      autofocus: false,
-      controller: passwordController,
-      obscureText: true,
-      
-      // validator: () {},
-      onSaved: (value)
-      {
-        passwordController.text = value!;
-      },
-      textInputAction: TextInputAction.done,
-      decoration: InputDecoration(
-        prefixIcon: Icon(Icons.vpn_key),
-        contentPadding: EdgeInsets.fromLTRB(20,15,20,15),
-        hintText: "Password",
-        border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(10),
+        autofocus: false,
+        controller: passwordController,
+        obscureText: true,
+
+        // validator: () {},
+        onSaved: (value) {
+          passwordController.text = value!;
+        },
+        textInputAction: TextInputAction.done,
+        decoration: InputDecoration(
+          prefixIcon: Icon(Icons.vpn_key),
+          contentPadding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+          hintText: "Password",
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
           ),
-
-      )
-    
-
-    );
-
+        ));
 
     final loginButton = Material(
       elevation: 5,
@@ -74,8 +66,16 @@ class _LoginScreenState extends State<LoginScreen> {
       color: Colors.blueAccent,
       child: MaterialButton(
         // Padding!
+        padding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+        minWidth: MediaQuery.of(context).size.width,
+
         onPressed: () {},
-        child: Text("Login"),
+        child: Text("Login",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+                fontSize: 20,
+                color: Colors.white,
+                fontWeight: FontWeight.bold)),
       ),
     );
 
@@ -85,21 +85,55 @@ class _LoginScreenState extends State<LoginScreen> {
         child: SingleChildScrollView(
           child: Container(
             color: Colors.white,
-            child: Form(
-              key: _formKey,
-              child: Column(
-                children: <Widget>[
-                  emailField,
-                  passwordField,
-                  loginButton
-                ],
+            child: Padding(
+              padding: const EdgeInsets.all(36.0),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    SizedBox(
+                        height: 200,
+                        child: Image.asset(
+                          "assets/logo.png",
+                          fit: BoxFit.contain,
+                        )),
+                    SizedBox(height: 45),
+                    emailField,
+                    SizedBox(height: 25),
+                    passwordField,
+                    SizedBox(height: 35),
+                    loginButton,
+                    SizedBox(height: 15),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text("Don't have an account? "),
+                        GestureDetector(
+                            onTap: () {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) =>
+                                          RegistrationScreen()));
+                            },
+                            child: Text(
+                              "SignUp",
+                              style: TextStyle(
+                                  color: Colors.blueAccent,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 15),
+                            ))
+                      ],
+                    )
+                  ],
+                ),
               ),
             ),
           ),
         ),
       ),
-
     );
   }
 }
-
--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -1,1 +1,24 @@
- 
+import 'package:flutter/material.dart';
+
+class RegistrationScreen extends StatefulWidget {
+  const RegistrationScreen({Key? key}) : super(key: key);
+
+  @override
+  _RegistrationScreenState createState() => _RegistrationScreenState();
+}
+
+class _RegistrationScreenState extends State<RegistrationScreen> {
+  // our form key
+  final _formKey = GlobalKey<FormState>();
+  // editing controller
+  final firstNameEditingController = new TextEditingController();
+  final secondNameEditingController = new TextEditingController();
+  final emailEditingController = new TextEditingController();
+  final passwordEditingController = new TextEditingController();
+  final confirmPasswordEditingController = new TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
